605b264f5f6934fd371b2b88773e526211e8568b
Dazmyt <kikwork1@gmail.com>
2021-10-28 15:36:02 +0300

    New print - добавил команды и перенос по строкам

--- main.py
+++ main.py
@@ -1,36 +1,37 @@
 print("привет мир")
-print("hi people,28,10,2021")
-# Пробуем дружить с гитом
-print("работает наконец командой")
-print("новая ветка")
-print("пуш ту главный:)")
-print("'ArithmeticError', 'AssertionError', 'AttributeError', 'BaseException',\
-\n'BlockingIOError', 'BrokenPipeError', 'BufferError', 'BytesWarning', 'ChildProcessError',\
-\n'ConnectionAbortedError', 'ConnectionError', 'ConnectionRefusedError',\
-\n'ConnectionResetError', 'DeprecationWarning', 'EOFError', 'Ellipsis', \
-\n'EncodingWarning', 'EnvironmentError', 'Exception', 'False',\
-\n'FileExistsError', 'FileNotFoundError', 'FloatingPointError',\
-\n'FutureWarning', 'GeneratorExit', 'IOError', 'ImportError', 'ImportWarning',\
-\n'IndentationError', 'IndexError', 'InterruptedError', 'IsADirectoryError',\
-\n'KeyError', 'KeyboardInterrupt', 'LookupError', 'MemoryError',\
-\n'ModuleNotFoundError', 'NameError', 'None', 'NotADirectoryError',\
-\n'NotImplemented', 'NotImplementedError', 'OSError', 'OverflowError',\
-\n'PendingDeprecationWarning', 'PermissionError', 'ProcessLookupError',\
-\n'RecursionError', 'ReferenceError', 'ResourceWarning', 'RuntimeError',\
-\n'RuntimeWarning', 'StopAsyncIteration', 'StopIteration', 'SyntaxError',\
-\n'SyntaxWarning', 'SystemError', 'SystemExit', 'TabError', 'TimeoutError',\
-\n'True', 'TypeError', 'UnboundLocalError', 'UnicodeDecodeError',\
-\n'UnicodeEncodeError', 'UnicodeError', 'UnicodeTranslateError',\
-\n'UnicodeWarning', 'UserWarning', 'ValueError', 'Warning', 'ZeroDivisionError',\
-\n'__build_class__', '__debug__', '__doc__', '__import__', '__loader__',\
-\n'__name__', '__package__', '__spec__', 'abs', 'aiter', 'all', 'anext',\
-\n'any', 'ascii', 'bin', 'bool', 'breakpoint', 'bytearray', 'bytes', 'callable',\
-\n'chr', 'classmethod', 'compile', 'complex', 'copyright', 'credits', 'delattr',\
-\n'dict', 'dir', 'divmod', 'enumerate', 'eval', 'exec', 'exit', 'filter', 'float',\
-\n'format', 'frozenset', 'getattr', 'globals', 'hasattr', 'hash', 'help', 'hex',\
-\n'id', 'input', 'int', 'isinstance', 'issubclass', 'iter', 'len', 'license',\
-\n'list', 'locals', 'map', 'max', 'memoryview', 'min', 'next', 'object', 'oct',\
-\n'open', 'ord', 'pow', 'print', 'property', 'quit', 'range', 'repr',\
-\n'reversed', 'round', 'set', 'setattr', 'slice', 'sorted', 'staticmethod',\
-\n'str', 'sum', 'super', 'tuple', 'type', 'vars', 'zip'")
-print("Команды готовы с переносом для красоты")
+# print("hi people,28,10,2021")
+# # Пробуем дружить с гитом
+# print("работает наконец командой")
+# print("новая ветка")
+# print("пуш ту главный:)")
+# print("'ArithmeticError', 'AssertionError', 'AttributeError', 'BaseException',\
+# \n'BlockingIOError', 'BrokenPipeError', 'BufferError', 'BytesWarning', 'ChildProcessError',\
+# \n'ConnectionAbortedError', 'ConnectionError', 'ConnectionRefusedError',\
+# \n'ConnectionResetError', 'DeprecationWarning', 'EOFError', 'Ellipsis', \
+# \n'EncodingWarning', 'EnvironmentError', 'Exception', 'False',\
+# \n'FileExistsError', 'FileNotFoundError', 'FloatingPointError',\
+# \n'FutureWarning', 'GeneratorExit', 'IOError', 'ImportError', 'ImportWarning',\
+# \n'IndentationError', 'IndexError', 'InterruptedError', 'IsADirectoryError',\
+# \n'KeyError', 'KeyboardInterrupt', 'LookupError', 'MemoryError',\
+# \n'ModuleNotFoundError', 'NameError', 'None', 'NotADirectoryError',\
+# \n'NotImplemented', 'NotImplementedError', 'OSError', 'OverflowError',\
+# \n'PendingDeprecationWarning', 'PermissionError', 'ProcessLookupError',\
+# \n'RecursionError', 'ReferenceError', 'ResourceWarning', 'RuntimeError',\
+# \n'RuntimeWarning', 'StopAsyncIteration', 'StopIteration', 'SyntaxError',\
+# \n'SyntaxWarning', 'SystemError', 'SystemExit', 'TabError', 'TimeoutError',\
+# \n'True', 'TypeError', 'UnboundLocalError', 'UnicodeDecodeError',\
+# \n'UnicodeEncodeError', 'UnicodeError', 'UnicodeTranslateError',\
+# \n'UnicodeWarning', 'UserWarning', 'ValueError', 'Warning', 'ZeroDivisionError',\
+# \n'__build_class__', '__debug__', '__doc__', '__import__', '__loader__',\
+# \n'__name__', '__package__', '__spec__', 'abs', 'aiter', 'all', 'anext',\
+# \n'any', 'ascii', 'bin', 'bool', 'breakpoint', 'bytearray', 'bytes', 'callable',\
+# \n'chr', 'classmethod', 'compile', 'complex', 'copyright', 'credits', 'delattr',\
+# \n'dict', 'dir', 'divmod', 'enumerate', 'eval', 'exec', 'exit', 'filter', 'float',\
+# \n'format', 'frozenset', 'getattr', 'globals', 'hasattr', 'hash', 'help', 'hex',\
+# \n'id', 'input', 'int', 'isinstance', 'issubclass', 'iter', 'len', 'license',\
+# \n'list', 'locals', 'map', 'max', 'memoryview', 'min', 'next', 'object', 'oct',\
+# \n'open', 'ord', 'pow', 'print', 'property', 'quit', 'range', 'repr',\
+# \n'reversed', 'round', 'set', 'setattr', 'slice', 'sorted', 'staticmethod',\
+# \n'str', 'sum', 'super', 'tuple', 'type', 'vars', 'zip'")
+# print("Команды готовы с переносом для красоты")
+print(dir (__builtins__))
