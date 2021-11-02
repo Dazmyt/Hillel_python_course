6ad6b21a513ea561db168ec0517ee5cfc33b62cb
Dazmyt <kikwork1@gmail.com>
2021-11-02 23:10:44 +0200

    Lesson4-test1

---
+++ Lesson4.py
@@ -0,0 +1,18 @@
+# Задание-1
+a = "Это строка в которую {} новую строку"
+input_string = input('Введите что-то')
+b = a.format(input_string)
+print(b)
+# задание-2
+str = b
+print(str.replace(input_string, "замена в строке"))
+c = str.replace(input_string, "замена в строке")
+# Длина строки
+print(len(c))
+# Итого да/нет
+string = c
+if "строка" in c:
+    print("ДА")
+else:
+    print("Нет")
+
