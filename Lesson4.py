0b73b3ccf3b680b1c6c1cd1672757678d20e809e
Dazmyt <kikwork1@gmail.com>
2021-11-03 15:33:02 +0200

    Lesson4-test1

--- Lesson4.py
+++ Lesson4.py
@@ -3,7 +3,7 @@ a = "Это строка в которую {} новую строку"
 input_string = input('Введите что-то')
 a = a.format(input_string)
 print(a)
-# задание-2
+# задание-2 почему лишний принт?
 print(a.replace(input_string, "замена в строке"))
 a = a.replace(input_string, "замена в строке")
 # Длина строки
